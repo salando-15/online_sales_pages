bb5da51d3ad8858fafee7e9c496a183527006fb4
Salando5 <saulorlandozzz@gmail.com>
2023-07-29 21:56:18 -0600

    feat: :rocket: Se añadio funcionalidad al nav,haciendo menu desplegable

--- js/mode.js
+++ js/mode.js
@@ -1,3 +1,4 @@
+const svgMode = document.getElementById("mode-button__svg")
 const mode = document.querySelector(".mode-button")
 const container = document.querySelector(".container")
 const eslogan = document.querySelector(".eslogan")
@@ -7,11 +8,13 @@ mode.addEventListener("click", () => {
   if (dark === "true"){
     container.style.background = "#F5F5F7"
     eslogan.style.color = "#121212"
+    svgMode.setAttribute("src", "svg/moon.svg")
     dark = "false"
   }else{
     container.style.background = "#121212"
     eslogan.style.color = "#F5F5F7"
     dark = "true"
+    svgMode.setAttribute("src", "svg/sun.svg")
   }
 })
 
