ee3ffcf8c6e0d215cfc318b018c59af4dd4b038b
Salando Linux <saulorlandozzz@gmail.com>
2023-07-31 08:25:05 -0600

    refactor: :adhesive_bandage: El modo oscuro o claro persiste en todas las paginas

--- js/mode.js
+++ js/mode.js
@@ -1,8 +1,21 @@
+"use strict"
+
 const svgMode = document.getElementById("mode-button__svg")
-const mode = document.querySelector(".mode-button")
-const container = document.querySelector(".container")
 const textToChangeColor = document.querySelectorAll(".text--change-color")
-let dark = "true"
+const modeButton = document.querySelector(".mode-button")
+const container = document.querySelector(".container")
+
+window.addEventListener("load", () => {
+  if (sessionStorage.getItem("mode") === null){
+    sessionStorage.setItem("mode", "light")
+  }
+
+  if (sessionStorage.getItem("mode") === "dark"){
+    changeColorAllText("#121212")
+    container.style.background = "#F5F5F7"
+    svgMode.setAttribute("src", "/svg/moon.svg")
+  }
+})
 
 function changeColorAllText(color){
   textToChangeColor.forEach(text => {
@@ -10,16 +23,16 @@ function changeColorAllText(color){
   })
 }
 
-mode.addEventListener("click", () => {
-  if (dark === "true"){
+modeButton.addEventListener("click", () => {
+  if (sessionStorage.getItem("mode") === "light"){
     changeColorAllText("#121212")
     container.style.background = "#F5F5F7"
-    dark = "false"
+    sessionStorage.setItem("mode", "dark")
     svgMode.setAttribute("src", "/svg/moon.svg")
   }else{
     container.style.background = "#121212"
     changeColorAllText("#F5F5F7")
-    dark = "true"
+    sessionStorage.setItem("mode", "light")
     svgMode.setAttribute("src", "/svg/sun.svg")
   }
 })
