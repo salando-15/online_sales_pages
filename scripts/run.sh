f875694c0a9a1aeb49af74ac0c62e8ae6fec9e7f
Salando Linux <saulorlandozzz@gmail.com>
2023-08-03 03:55:54 -0600

    feat: :dizzy: se maqueto la pagina de hp y se añadio un nueva animacion de hover a las cards de productos

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,4 +2,5 @@
 
 pug index.pug
 # pug html/shoppingCart.pug && pug html/macbooks.pug && pug html/iPhone.pug
-pug html/xiaomis.pug && pug html/googlePixel.pug && pug html/dells.pug
+# pug html/xiaomis.pug && pug html/googlePixel.pug && pug html/dells.pug
+pug html/hps.pug
