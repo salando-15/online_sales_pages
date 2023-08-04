2540e25e81731afd14d9b510e5aebd83b865205e
Salando Linux <saulorlandozzz@gmail.com>
2023-08-04 14:11:24 -0600

    feat: :rocket: se maquetaron las paginas de insttrumenos(nav)

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 pug index.pug
-# pug html/shoppingCart.pug && pug html/macbooks.pug && pug html/iPhone.pug
-# pug html/xiaomis.pug && pug html/googlePixel.pug && pug html/dells.pug
-pug html/hps.pug
+# pug html/shoppingCart.pug
+
+# Nav
+# pug html/macbooks.pug && pug html/dells.pug && pug html/hps.pug
+# pug html/iPhone.pug && pug html/xiaomis.pug && pug html/googlePixel.pug
+pug html/instrumentosDeCuerda.pug && pug html/viento.pug
