9113d14528f76429fb00f014622ece02b196860c
Salando Linux <saulorlandozzz@gmail.com>
2023-08-05 14:48:47 -0600

    feat: :rocket: se maquetaron todas las paginas del carrusel

--- scripts/run.sh
+++ scripts/run.sh
@@ -7,4 +7,7 @@ pug index.pug
 # pug html/macbooks.pug && pug html/dells.pug && pug html/hps.pug
 # pug html/iPhone.pug && pug html/xiaomis.pug && pug html/googlePixel.pug
 # pug html/instrumentosDeCuerda.pug && pug html/viento.pug
-pug html/teclados.pug && pug html/audifonos.pug && pug html/impresoras.pug && pug html/bocinas.pug
+# pug html/teclados.pug && pug html/audifonos.pug && pug html/impresoras.pug && pug html/bocinas.pug
+
+# Carrusel
+pug html/accesoriosVideojuegos.pug && pug html/ofertas.pug && pug html/popular.pug
