b042a56e1dc774acaad8f6390cec8331677065ef
Salando Linux <saulorlandozzz@gmail.com>
2023-08-05 00:09:10 -0600

    feat: se maqueto la pagina de teclados

--- scripts/run.sh
+++ scripts/run.sh
@@ -6,4 +6,5 @@ pug index.pug
 # Nav
 # pug html/macbooks.pug && pug html/dells.pug && pug html/hps.pug
 # pug html/iPhone.pug && pug html/xiaomis.pug && pug html/googlePixel.pug
-pug html/instrumentosDeCuerda.pug && pug html/viento.pug
+# pug html/instrumentosDeCuerda.pug && pug html/viento.pug
+pug html/teclados.pug
