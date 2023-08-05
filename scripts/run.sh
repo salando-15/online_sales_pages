79277e0541371446b3f6f4a517ca6e25d47fe493
Salando Linux <saulorlandozzz@gmail.com>
2023-08-05 03:07:04 -0600

    feat: :rocket: se maqueto la pagina de impresoras

--- scripts/run.sh
+++ scripts/run.sh
@@ -7,4 +7,4 @@ pug index.pug
 # pug html/macbooks.pug && pug html/dells.pug && pug html/hps.pug
 # pug html/iPhone.pug && pug html/xiaomis.pug && pug html/googlePixel.pug
 # pug html/instrumentosDeCuerda.pug && pug html/viento.pug
-pug html/teclados.pug && pug html/audifonos.pug
+pug html/teclados.pug && pug html/audifonos.pug && pug html/impresoras.pug
